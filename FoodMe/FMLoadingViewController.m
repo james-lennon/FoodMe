83dfc179f952ffc59cf20f81a03ab65772fa8d7a
James Lennon <jameslennon321@gmail.com>
2015-11-14 18:35:34 -0500

    Finished FMLoadingViewController

--- FoodMe/FMLoadingViewController.m
+++ FoodMe/FMLoadingViewController.m
@@ -8,6 +8,8 @@
 
 #import "FMLoadingViewController.h"
 #import "FMLoadingView.h"
+#import "FMLabel.h"
+#import "FMDataLoader.h"
 #import "FMColors.h"
 
 @implementation FMLoadingViewController
@@ -17,6 +19,15 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     self.view.backgroundColor = BACKGROUND_COLOR;
+    
+    CGFloat padding = 50;
+    CGFloat width = self.view.frame.size.width;
+    
+    FMLabel* label = [[FMLabel alloc] initWithFrame:CGRectMake(padding / 3, padding / 3, width - 2.0 / 3.0 * padding, 200)];
+    NSString* loadingText = [NSString stringWithFormat:@"%@%@", [[FMDataLoader sharedInstance] genLoadingMessage], @"..."];
+    
+    [label setText:loadingText];
+    [self.view addSubview:label];
     
     CGSize size = self.view.frame.size;
     FMLoadingView* lv = [[FMLoadingView alloc] initWithFrame:CGRectMake(size.width / 2 - 120, size.height / 2 - 100, 200, 200)];
