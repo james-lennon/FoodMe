f2faea924f8f4647a9ce1703f9baf3459d36adbf
James Lennon <jameslennon321@gmail.com>
2015-11-14 11:15:21 -0500

    Fixed font stuff

---
+++ FoodMe/FMMainViewController.m
@@ -0,0 +1,27 @@
+//
+//  FMMainViewController.m
+//  FoodMe
+//
+//  Created by James Lennon on 11/14/15.
+//  Copyright © 2015 Jake Saferstein. All rights reserved.
+//
+
+#import "FMMainViewController.h"
+#import "FMColors.h"
+
+@implementation FMMainViewController
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+
+-(void)viewDidLoad {
+    self.view.backgroundColor = BACKGROUND_COLOR;
+}
+
+@end
