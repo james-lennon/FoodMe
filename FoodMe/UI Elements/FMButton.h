89d712c1fa65d149bc46ba2ade7849d8f5cad5e7
James Lennon <jameslennon321@gmail.com>
2015-11-14 00:41:20 -0500

    Added FMButton class

---
+++ FoodMe/UI Elements/FMButton.h
@@ -0,0 +1,13 @@
+//
+//  FMButton.h
+//  FoodMe
+//
+//  Created by James Lennon on 11/14/15.
+//  Copyright © 2015 Jake Saferstein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FMButton : UIButton
+
+@end
---
+++ FoodMe/UI Elements/FMButton.m
@@ -0,0 +1,21 @@
+//
+//  FMButton.m
+//  FoodMe
+//
+//  Created by James Lennon on 11/14/15.
+//  Copyright © 2015 Jake Saferstein. All rights reserved.
+//
+
+#import "FMButton.h"
+
+@implementation FMButton
+
+-(id)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+    }
+    return self;
+}
+
+@end
