6ecd4e5ca9cafe7a66dddc4d055470c911e7ffc2
Jake Saferstein <jakesafo@gmail.com>
2015-11-14 11:32:11 -0500

    Added loading/question files

---
+++ FoodMe/FMDataLoader.h
@@ -0,0 +1,16 @@
+//
+//  FMDataLoader.h
+//
+//
+//  Created by Marko Fejzo on 11/14/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FMDataLoader : NSObject
+
+- (NSMutableArray *) genQuestion;
+- (NSString *) genLoadingMessage;
+
+@end
